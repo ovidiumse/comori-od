f355e512214039010d93ebed553e86e710e6c740
Ovidiu Moscaliuc <moscaliuc_ovidiu@yahoo.com>
2020-08-05 13:05:35 +0300

    Made od-extract.py script configurable

--- tools/od-extract.py
+++ tools/od-extract.py
@@ -4,23 +4,11 @@ import os
 import re
 import argparse
 import simplejson as json
+import yaml
 from datetime import datetime
 from bs4 import BeautifulSoup
 
 PARSER_ = argparse.ArgumentParser(description="OD content extractor.")
-
-TITLE_FSIZE = "250%"
-TITLE_COLOR = "#1a8cff"
-
-SUBTITLE_FSIZE = "250%"
-SUBTITLE_COLOR = "#ff0000"
-
-ARTICLE_TITLE_FSIZE = "208%"
-ARTICLE_TITLE_COLOR = "#008000"
-ARTICLE_BODY_FSIZE = "125%"
-
-POEM_TITLE_FSIZE = "166%"
-POEM_TITLE_COLOR = "#ff00ff"
 
 
 def parseArgs():
@@ -31,7 +19,13 @@ def parseArgs():
                          type=str,
                          required=True,
                          help="Input html file")
-    PARSER_.add_argument("-t", "--title", dest="title", action="store", type=str, help="Book title")
+    PARSER_.add_argument("-c",
+                         "--cfg",
+                         dest="cfg",
+                         action="store",
+                         type=str,
+                         required=True,
+                         help="Parsing config")
     PARSER_.add_argument("-a",
                          "--author",
                          dest="author",
@@ -39,18 +33,25 @@ def parseArgs():
                          type=str,
                          required=True,
                          help="Book author")
-    PARSER_.add_argument("-pt",
-                         "--print-title",
-                         dest="print_title",
+    PARSER_.add_argument("-v",
+                         "--volume",
+                         dest="volume",
+                         type=str,
+                         default=None,
+                         help="Volume title")
+    PARSER_.add_argument("-b", "--book", dest="book", type=str, default=None, help="Book title")
+    PARSER_.add_argument("-pv",
+                         "--print-volumes",
+                         dest="print_volumes",
                          action="store_true",
                          default=False,
-                         help="Print title")
-    PARSER_.add_argument("-ps",
-                         "--print-subtitles",
-                         dest="print_subtitles",
+                         help="Print volume")
+    PARSER_.add_argument("-pb",
+                         "--print-books",
+                         dest="print_books",
                          action="store_true",
                          default=False,
-                         help="Print subtitles")
+                         help="Print books")
     PARSER_.add_argument("-pat",
                          "--print-article-titles",
                          dest="print_article_titles",
@@ -72,13 +73,13 @@ def parseArgs():
     return PARSER_.parse_args()
 
 
-def hasStyleAtribute(tag, attr, value):
+def hasStyleAttribute(tag, attr, value):
     return tag.has_attr('style') and "{}: {}".format(attr, value) in tag['style']
 
 
 def hasNestedStyleAttribute(tag, attr, value):
     for d in tag.descendants:
-        if d.name == 'span' and hasStyleAtribute(d, attr, value):
+        if d.name == 'span' and hasStyleAttribute(d, attr, value):
             return True
 
     return False
@@ -86,7 +87,7 @@ def hasNestedStyleAttribute(tag, attr, value):
 
 def getNestedStyleAttribute(tag, attr):
     for d in tag.descendants:
-        if d.name == 'span' and tag.has_attr('style') and attr in d['style']:
+        if d.name == 'span' and d.has_attr('style') and attr in d['style']:
             props = d['style'].split(';')
             props = [prop for prop in props if attr in prop]
             prop = props[0]
@@ -109,56 +110,106 @@ def getFontSize(tag):
     if not match:
         return 0
     else:
-        assert(len(match) == 1)
+        assert (len(match) == 1)
         return int(match[0])
 
-def isTitle(tag):
-    return tag.name == "p" and hasFontSize(tag, TITLE_FSIZE) and hasColor(tag, TITLE_COLOR)
+
+def hasLetterSpacing(text, spacing):
+    results = re.findall(r'(\w\w)+', text)
+    if spacing == "none" and results:
+        return True
+    elif spacing == 1 and not results:
+        return True
+
+    return False
 
 
-def isSubtitle(tag):
-    return tag.name == "p" and hasFontSize(tag, SUBTITLE_FSIZE) and hasColor(tag, SUBTITLE_COLOR)
+def isCapitalized(text):
+    results = re.findall(r'[a-z]+', text)
+    return not results
 
 
-def isArticleTitle(tag):
-    return tag.name == "p" and hasFontSize(tag, ARTICLE_TITLE_FSIZE) and hasColor(
-        tag, ARTICLE_TITLE_COLOR) and hasStyleAtribute(tag, 'text-align', 'justify')
+def checkProps(tag, cfg):
+    if 'nested-style' in cfg:
+        for p, v in cfg['nested-style'].items():
+            if not hasNestedStyleAttribute(tag, p, v):
+                return False
+
+    if 'style' in cfg:
+        for p, v in cfg['style'].items():
+            if not hasStyleAttribute(tag, p, v):
+                return False
+
+    if 'other' in cfg:
+        for p, v in cfg['other'].items():
+            if p == 'letter-spacing':
+                if not hasLetterSpacing(tag.text, v):
+                    return False
+            elif p == 'capitalize':
+                if v != isCapitalized(tag.text):
+                    return False
+
+    return True
 
 
-def isPoemTitle(tag):
-    return tag.name == "p" and hasFontSize(tag, POEM_TITLE_FSIZE) and hasColor(
-        tag, POEM_TITLE_COLOR) and hasStyleAtribute(tag, 'text-align', 'justify')
+def isVolumeTitle(tag, cfg):
+    if tag.name != "p":
+        return False
+
+    return checkProps(tag, cfg['volume-title'])
 
 
-def printElements(soup, predicate):
-    for p in soup.find_all(predicate):
+def isBookTitle(tag, cfg):
+    if tag.name != "p":
+        return False
+
+    return checkProps(tag, cfg['book-title'])
+
+
+def isArticleTitle(tag, cfg):
+    if tag.name != "p":
+        return False
+
+    return checkProps(tag, cfg['article-title'])
+
+
+def isPoemTitle(tag, cfg):
+    if tag.name != "p":
+        return False
+
+    return checkProps(tag, cfg['poem-title'])
+
+
+def printElements(soup, predicate, cfg):
+    for p in soup.find_all(lambda tag: predicate(tag, cfg)):
         print(p.text)
 
 
-def printTitles(soup):
-    printElements(soup, isTitle)
+def printVolumeTitles(soup, cfg):
+    printElements(soup, isVolumeTitle, cfg)
 
 
-def printSubtitles(soup):
-    printElements(soup, isSubtitle)
+def printBookTitles(soup, cfg):
+    printElements(soup, isBookTitle, cfg)
 
 
-def printArticleTitles(soup):
-    printElements(soup, isArticleTitle)
+def printArticleTitles(soup, cfg):
+    printElements(soup, isArticleTitle, cfg)
 
 
-def printPoemTitles(soup):
-    printElements(soup, isPoemTitle)
+def printPoemTitles(soup, cfg):
+    printElements(soup, isPoemTitle, cfg)
 
-def extractArticles(soup, volume, author):
+
+def extractArticles(soup, volume, book, author, cfg):
     articles = []
-    book = volume
+
     for p in soup.find_all('p'):
-        if isSubtitle(p):
+        if not book and isBookTitle(p, cfg):
             book = p.text
-        elif isArticleTitle(p) or isPoemTitle(p):
+        elif isArticleTitle(p, cfg) or isPoemTitle(p, cfg):
             title = p.text
-            type = "poezie" if isPoemTitle(p) else "articol"
+            type = "poezie" if isPoemTitle(p, cfg) else "articol"
             verses = []
             lastTag = ""
             lastValue = ""
@@ -194,6 +245,10 @@ def extractArticles(soup, volume, author):
 def main():
     args = parseArgs()
 
+    cfg = {}
+    with open(args.cfg, 'r') as cfg_file:
+        cfg = yaml.full_load(cfg_file)
+
     with open(args.html_filepath, 'r', encoding='utf-8') as html_file:
         start = datetime.now()
         print("Parsing {}...".format(args.html_filepath))
@@ -202,24 +257,24 @@ def main():
 
         print("Parsing done in {}.".format(parse_finish - start))
 
-        p = soup.find(isTitle)
-        volume = p.text
+        volume = args.volume if args.volume else soup.find(
+            lambda tag: isVolumeTitle(tag, cfg)).text
 
-        if args.print_title:
-            printTitles(soup)
-        if args.print_subtitles:
-            printSubtitles(soup)
+        if args.print_volumes:
+            printVolumeTitles(soup, cfg)
+        if args.print_books:
+            printBookTitles(soup, cfg)
         if args.print_article_titles:
-            printArticleTitles(soup)
+            printArticleTitles(soup, cfg)
         if args.print_poem_titles:
-            printPoemTitles(soup)
+            printPoemTitles(soup, cfg)
         if args.extract_filename:
             print("Extracting all articles...")
-            articles = extractArticles(soup, volume, args.author)
+            articles = extractArticles(soup, volume, args.book, args.author, cfg)
             extract_finish = datetime.now()
 
-            print("Extracted {} articles in {}.".
-                  format(len(articles), extract_finish - parse_finish))
+            print("Extracted {} articles in {}.".format(len(articles),
+                                                        extract_finish - parse_finish))
 
             print("Writing {} articles to {}...".format(len(articles), args.extract_filename))
             with open(args.extract_filename, 'w') as articles_file:
@@ -238,8 +293,6 @@ def main():
                         print(verse, file=text_file)
 
                     print("", file=text_file)
-
-
 
         finish = datetime.now()
         print("All done in {}.".format(finish - start))
