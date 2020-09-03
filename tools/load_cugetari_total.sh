af0ee8d56817eb5b187a212856704bb95cf4fc87
Ovidiu Moscaliuc <moscaliuc_ovidiu@yahoo.com>
2020-09-03 12:39:05 +0300

    Adding fixing step for Cugetari Nemuritoare

--- tools/load_cugetari_total.sh
+++ tools/load_cugetari_total.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-host_type=${1:-}
-
+echo "Fixing Cugetari Nemuritoare..."
+./od-fix.py -i ../data/cugetari_total/cugetari_total.htm -c ../cfg/cugetari_total.yaml
 echo "Extracting Cugetari Nemuritoare..."
-./od-extract.py -i ../data/cugetari_total.htm -c ../cfg/cugetari_total.yaml -a "Traian Dorz" -e ../data/cugetari_total.json
+./od-extract.py -i ../data/cugetari_total/cugetari_total_fixed.htm -c ../cfg/cugetari_total.yaml -a "Traian Dorz" -e ../data/cugetari_total/cugetari_total.json
 echo "Post-processing Cugetari Nemuritoare..."
-./od_postprocess/od_postprocess.py -i ../data/cugetari_total.json
-echo "Uploading Cugetari Nemuritoare using $host_type flag..."
-./od-upload.py -i ../data/cugetari_total_processed.json --index od $host_type
+./od_postprocess/od_postprocess.py -i ../data/cugetari_total/cugetari_total.json
+echo "Uploading Cugetari Nemuritoare using '$@' flags..."
+./od-upload.py -i ../data/cugetari_total/cugetari_total_processed.json --index od $@
