9838878909835450f7aaac8f94705c2ade090482
Ovidiu Moscaliuc <moscaliuc_ovidiu@yahoo.com>
2020-10-02 12:01:52 +0300

    od-fix improvements

--- tools/od-fix.py
+++ tools/od-fix.py
@@ -3,6 +3,7 @@ import os
 import re
 import argparse
 import yaml
+import time
 from datetime import datetime
 from bs4 import BeautifulSoup
 
@@ -30,7 +31,17 @@ def parseArgs():
 
 
 def hasStyleAttribute(tag, attr, value):
-    return tag.has_attr('style') and "{}: {}".format(attr, value) in tag['style']
+    if not tag.has_attr('style'):
+        return False
+
+    if value == '*':
+        return "{}: ".format(attr) in tag['style']
+    else:
+        return "{}: {}".format(attr, value) in tag['style']
+
+
+def hasClassAttribute(tag, value):
+    return tag.has_attr('class') and value in tag['class']
 
 
 def hasNestedStyleAttribute(tag, attr, value):
@@ -48,13 +59,35 @@ def replaceNestedStyleAttribute(tag, attr, value):
 
 
 def replaceStyleAttribute(tag, attr, value):
+    if tag.has_attr('style'):
+        styles_text = tag['style']
+        style_pairs = styles_text.split(';')
+        new_style = {}
+        for pair in style_pairs:
+            p, v = pair.split(':')
+            new_style[p.strip()] = value if attr == p.strip() else v.strip()
+
+        if attr not in tag['style']:
+            new_style[attr] = value
+
+        tag['style'] = ";".join(["{}: {}".format(k, v) for k, v in new_style.items()])
+
+
+def removeNestedStyleAttribute(tag, attr):
+    for d in tag.descendants:
+        if d.name == 'span':
+            removeStyleAttribute(d, attr)
+
+
+def removeStyleAttribute(tag, attr):
     if tag.has_attr('style') and attr in tag['style']:
         styles_text = tag['style']
         style_pairs = styles_text.split(';')
         new_style = {}
         for pair in style_pairs:
             p, v = pair.split(':')
-            new_style[p] = value if attr == p else v
+            if p.strip() != attr:
+                new_style[p.strip()] = v.strip()
 
         tag['style'] = ";".join(["{}: {}".format(k, v) for k, v in new_style.items()])
 
@@ -68,7 +101,19 @@ def replaceProps(tag, cfg):
         for prop, val in cfg['style'].items():
             replaceStyleAttribute(tag, prop, val)
 
+
+def removeProps(tag, cfg):
+    if 'nested-style' in cfg:
+        for prop, val in cfg['nested-style'].items():
+            removeNestedStyleAttribute(tag, prop)
+
+    if 'style' in cfg:
+        for prop, val in cfg['style'].items():
+            removeStyleAttribute(tag, prop)
+
+
 def checkProps(tag, cfg):
+    fl = False
     if 'nested-style' in cfg:
         for p, v in cfg['nested-style'].items():
             if not hasNestedStyleAttribute(tag, p, v):
@@ -78,6 +123,12 @@ def checkProps(tag, cfg):
         for p, v in cfg['style'].items():
             if not hasStyleAttribute(tag, p, v):
                 return False
+
+    if 'class' in cfg:
+        if not hasClassAttribute(tag, cfg['class']):
+            return False
+        else:
+            fl = True
 
     if 'other' in cfg:
         for p, v in cfg['other'].items():
@@ -105,17 +156,33 @@ def isBeforeArticleTitle(tag, cfg):
     return checkProps(tag, cfg['pre-before-article-title'])
 
 
-def isArticleTitle(tag, cfg):
-    if 'pre-article-title' not in cfg:
+def isPreArticleTitle(tag, cfg):
+    props = None
+    if 'pre-article-title' in cfg:
+        props = cfg['pre-article-title']
+    elif 'article-title' in cfg:
+        props = cfg['article-title']
+
+    if not props:
         return False
 
     if tag.name != "p":
         return False
 
-    return checkProps(tag, cfg['pre-article-title'])
+    return checkProps(tag, props)
 
 
-def isArticleSubtitle(tag, cfg):
+def isPrePoemTitle(tag, cfg):
+    if 'poem-title' not in cfg:
+        return False
+
+    if tag.name != "p":
+        return False
+
+    return checkProps(tag, cfg['poem-title'])
+
+
+def isPreArticleSubtitle(tag, cfg):
     if 'pre-article-subtitle' not in cfg:
         return False
 
@@ -139,6 +206,10 @@ def isWhiteText(tag, cfg):
     return hasStyleAttribute(tag, 'color', '#ffffff') or hasStyleAttribute(tag, 'color', '#FFFFFF')
 
 
+def isHardPageBreak(tag, cfg):
+    return tag.name == "div" and tag.has_attr('class') and 'WPHardPageBreak' in tag['class']
+
+
 def printElements(soup, predicate):
     for p in soup.find_all(predicate):
         print(p.text)
@@ -151,18 +222,18 @@ def find_title(soup, cfg, last_title=None):
             if not pre_title:
                 return None
 
-            title = pre_title.find_next(lambda tag: isArticleTitle(tag, cfg))
+            title = pre_title.find_next(lambda tag: isPreArticleTitle(tag, cfg))
         else:
-            title = last_title.find_next(lambda tag: isArticleTitle(tag, cfg))
+            title = last_title.find_next(lambda tag: isPreArticleTitle(tag, cfg))
     else:
         if 'pre-before-article-title' in cfg:
             pre_title = soup.find(lambda tag: isBeforeArticleTitle(tag, cfg))
             if not pre_title:
                 return None
 
-            title = pre_title.find_next(lambda tag: isArticleTitle(tag, cfg))
+            title = pre_title.find_next(lambda tag: isPreArticleTitle(tag, cfg))
         else:
-            title = soup.find(lambda tag: isArticleTitle(tag, cfg))
+            title = soup.find(lambda tag: isPreArticleTitle(tag, cfg))
 
     return title
 
@@ -179,7 +250,7 @@ def merge_multiline_titles(soup, cfg):
     title = find_title(soup, cfg)
     while title:
         next_p = title.find_next("p")
-        if isArticleTitle(next_p, cfg):
+        if isPreArticleTitle(next_p, cfg):
             print("Merging {} with {}...".format(title.text, next_p.text))
             title.append(next_p.text)
             next_p.decompose()
@@ -193,7 +264,7 @@ def merge_titles_with_subtitles(soup, cfg):
     title = find_title(soup, cfg)
     while title:
         next_p = title.find_next("p")
-        if isArticleSubtitle(next_p, cfg):
+        if isPreArticleSubtitle(next_p, cfg):
             print("Merging {} with {}...".format(title.text, next_p.text))
             title.append(" - {}".format(sanitize_subtitle(next_p.text)))
             next_p.decompose()
@@ -201,8 +272,8 @@ def merge_titles_with_subtitles(soup, cfg):
         title = find_title(soup, cfg, title)
 
 
-def increate_title_sizes(soup, cfg):
-    print("Increasing title sizes...")
+def normalize_title_sizes(soup, cfg):
+    print("Normalizing title sizes...")
 
     title = find_title(soup, cfg)
     while title:
@@ -212,17 +283,23 @@ def increate_title_sizes(soup, cfg):
 
 def fix_first_paragraph_first_letters(soup, cfg):
     print("Fixing first paragraph first letters...")
+
     letters = soup.find_all(lambda tag: isFirstParagraphFirstLetter(tag, cfg))
     for letter in letters:
-        parent = letter.find_parent()
+        parent = letter.find_parent("p")
+
+        # remove all unnecessary spaces
         for d in parent.find_all():
             if len(d.get_text(strip=True)) == 0:
                 d.decompose()
+
+        # find first string larger than 1, and prepend the letter to it
         for d in parent.find_all(string=True):
             if len(d.string) > 1:
-                d.string.replace_with(letter.text.strip() + d.string.strip())
+                d.string.replace_with(letter.text.rstrip() + d.string.lstrip())
                 break
 
+        # destroy the letter element
         letter.decompose()
 
 
@@ -232,6 +309,15 @@ def remove_white_text(soup, cfg):
     for t in texts:
         for d in t.find_all(string=True):
             d.string.replace_with("")
+
+
+def replace_hardpagebreaks_with_breaks(soup, cfg):
+    print("Removing hard breaks...")
+    breaks = soup.find_all(lambda tag: isHardPageBreak(tag, cfg))
+    for b in breaks:
+        newBreak = soup.new_tag("br")
+        b.insert_before(newBreak)
+        b.decompose()
 
 
 def hasCfg(tag, elementCfg):
@@ -253,6 +339,22 @@ def preprocess(soup, cfg):
         preprocessApply(soup, c['to'], c['apply'])
 
 
+def sanitize_adjacent_titles(soup, cfg):
+    all_titles = soup.find_all(lambda tag: isPreArticleTitle(tag, cfg) or isPrePoemTitle(tag, cfg))
+    lastTitle = None
+    for p in all_titles:
+        if not lastTitle:
+            lastTitle = p
+            continue
+
+        if isPrePoemTitle(lastTitle, cfg) and isPrePoemTitle(p, cfg):
+            props = {'style': {'margin-left': '*'}}
+            if (not checkProps(lastTitle, props) and checkProps(p, props)):
+                removeProps(p, props)
+
+        lastTitle = p
+
+
 def main():
     args = parseArgs()
 
@@ -266,6 +368,8 @@ def main():
         soup = BeautifulSoup(html_file, 'html.parser')
         parse_finish = datetime.now()
 
+        sanitize_adjacent_titles(soup, cfg)
+
         if 'preprocessing' in cfg:
             preprocess(soup, cfg)
 
@@ -274,9 +378,10 @@ def main():
         if 'pre-article-subtitle' in cfg:
             merge_titles_with_subtitles(soup, cfg)
 
-        increate_title_sizes(soup, cfg)
+        normalize_title_sizes(soup, cfg)
         fix_first_paragraph_first_letters(soup, cfg)
         remove_white_text(soup, cfg)
+        replace_hardpagebreaks_with_breaks(soup, cfg)
 
         path, ext = os.path.splitext(args.html_filepath)
         fixed_filepath = "{}_fixed{}".format(path, ext)
