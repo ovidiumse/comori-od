c74949e891dc4ec664ce33c3bffe32a81877bdd1
Ovidiu Moscaliuc <moscaliuc_ovidiu@yahoo.com>
2020-10-02 11:13:36 +0300

    Adding fix stage for Cugetari Nemuritoare

--- tools/load_cugetari_total.sh
+++ tools/load_cugetari_total.sh
@@ -2,9 +2,12 @@
 
 echo "Fixing Cugetari Nemuritoare..."
 ./od-fix.py -i ../data/cugetari_total/cugetari_total.htm -c ../cfg/cugetari_total.yaml
+
 echo "Extracting Cugetari Nemuritoare..."
 ./od-extract.py -i ../data/cugetari_total/cugetari_total_fixed.htm -c ../cfg/cugetari_total.yaml -a "Traian Dorz" -e ../data/cugetari_total/cugetari_total.json
+
 echo "Post-processing Cugetari Nemuritoare..."
 ./od_postprocess/od_postprocess.py -i ../data/cugetari_total/cugetari_total.json
+
 echo "Uploading Cugetari Nemuritoare using '$@' flags..."
 ./od-upload.py -i ../data/cugetari_total/cugetari_total_processed.json --index od $@
