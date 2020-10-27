334e738fe8edabb8c27dcdef9168cc53572485f5
Ovidiu Moscaliuc <moscaliuc_ovidiu@yahoo.com>
2020-10-27 22:15:13 +0200

    Added configurable return fields to od-search.py

--- tools/od-search.py
+++ tools/od-search.py
@@ -7,12 +7,10 @@ import logging
 
 PARSER_ = argparse.ArgumentParser(description="OD content suggester.")
 
-EXTERNAL_HOST = "vps-4864b0cc.vps.ovh.net:8000"
-LOCAL_HOST = "localhost:9000"
+EXTERNAL_HOST = "https://api.comori-od.ro"
+LOCAL_HOST = "http://localhost/api"
 
 COMORI_OD_API_HOST = LOCAL_HOST
-COMORI_OD_API_BASEURL = "http://{}".format(COMORI_OD_API_HOST)
-
 
 def parseArgs():
     PARSER_.add_argument("-i",
@@ -21,34 +19,59 @@ def parseArgs():
                          dest="idx_name",
                          default="od",
                          help="Index name")
-    PARSER_.add_argument("-t", "--doc-type", action="store", required=True, help="Document type")
+    PARSER_.add_argument("-t", "--doc-type", action="store", default="articles", help="Document type")
     PARSER_.add_argument("-e", "--external-host", action="store_true", help="Query external host")
+    PARSER_.add_argument("-f",
+                         "--fields",
+                         action="store",
+                         default=[
+                             "hits", "id", "score", "volume", "book", "title",
+                             "type", "author", "highlight"
+                         ],
+                         nargs='+')
     PARSER_.add_argument("query", action="store", help="Query")
 
     return PARSER_.parse_args()
 
 
 def get(uri):
-    response = requests.get("{}/{}".format(COMORI_OD_API_BASEURL, uri))
+    response = requests.get("{}/{}".format(COMORI_OD_API_HOST, uri))
     if response.status_code != requests.codes.ok:
         logging.error("Error: {}".format(json.dumps(response.json(), indent=2)))
     response.raise_for_status()
     return response.json()
 
 
-def search(idx_name, doc_type, query):
+def search(idx_name, doc_type, query, fields):
     results = get("{}/{}?q={}".format(idx_name, doc_type, query))
-    print("Got {} hits:".format(results['hits']['total']['value']))
+    if "hits" in fields:
+        print("Got {} hits:".format(results['hits']['total']['value']))
+
     for hit in results['hits']['hits']:
         result = hit['_source']
-        print("Id: {}".format(hit['_id']))
-        print("Score: {}".format(hit['_score']))
-        print("Volume: {}".format(result['volume']))
-        print("Book: {}".format(result['book']))
-        print("Title: {}".format(result['title']))
-        print("Type: {}".format(result['type']))
-        print("Author: {}".format(result['author']))
-        if 'highlight' in hit:
+    
+        if "id" in fields:
+            print("Id: {}".format(hit['_id']))
+
+        if "score" in fields:
+            print("Score: {}".format(hit['_score']))
+        
+        if "volume" in fields:
+            print("Volume: {}".format(result['volume']))
+
+        if "book" in fields:
+            print("Book: {}".format(result['book']))
+
+        if "title" in fields:
+            print("Title: {}".format(result['title']))
+
+        if "type" in fields:
+            print("Type: {}".format(result['type']))
+
+        if "author" in fields:
+            print("Author: {}".format(result['author']))
+
+        if "highlight" in fields and 'highlight' in hit:
             print("highlight: {}".format(json.dumps(hit['highlight'], indent=2)))
         print("")
 
@@ -56,14 +79,13 @@ def search(idx_name, doc_type, query):
 def main():
     args = parseArgs()
 
-    global COMORI_OD_API_HOST, COMORI_OD_API_BASEURL
+    global COMORI_OD_API_HOST
 
     if args.external_host:
         COMORI_OD_API_HOST = EXTERNAL_HOST
-        COMORI_OD_API_BASEURL = "http://{}".format(COMORI_OD_API_HOST)
 
-    print("Searching {} using {}...".format(args.query, COMORI_OD_API_BASEURL))
-    search(args.idx_name, args.doc_type, args.query)
+    print("Searching {} using {}...".format(args.query, COMORI_OD_API_HOST))
+    search(args.idx_name, args.doc_type, args.query, args.fields)
 
 
 if "__main__" == __name__:
