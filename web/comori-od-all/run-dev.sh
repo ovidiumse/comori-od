39c1b97af533e3dddb1204d202842409b57c68d5
Ovidiu Moscaliuc <moscaliuc_ovidiu@yahoo.com>
2021-04-16 09:50:46 +0300

    Added Jupyter docker container, improved dev startup script

--- web/comori-od-all/run-dev.sh
+++ web/comori-od-all/run-dev.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/sh
 
+echo "Starting services..."
 docker-compose -f docker-compose-dev.yaml --env-file .env-dev up -d
+if [ "$1" != "all" ]; then
+    echo "Stopping extra (optional) services..."
+    docker stop comori-od-filebeat comori-od-logstash comori-od-metricbeat comori-od-kibana
+fi
