53e4c0d16fcdd710e089ba336a8b5484308b065a
Ovidiu Moscaliuc <moscaliuc_ovidiu@yahoo.com>
2021-05-20 13:50:42 +0300

    Adding script for preparing output diffs

---
+++ tools/prepare_diff.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+DEST="/mnt/c/#work/data"
+mkdir -p $DEST
+
+CWD=`realpath $(dirname $0)`
+
+copy() {
+    source=$1
+    dest=$2
+
+    for file in $source/*.txt; do
+        echo "Copying $file to $dest"
+        cp $file $dest
+    done
+
+    for file in $source/*.json; do
+        echo "Copying $file to $dest"
+        cp $file $dest
+    done
+}
+
+prepare_dir() {
+    dir=$CWD/$1
+    dest_old=$DEST/old/$1/
+    dest_new=$DEST/new/$1/
+
+    mkdir -p $dest_old
+    mkdir -p $dest_new
+
+    copy $dir $dest_new
+
+    echo "Checking out $dir/*.txt from HEAD..."
+    git checkout $dir/*.txt
+    
+    echo "Checking out $dir/*.json from HEAD..."
+    git checkout $dir/*.json
+
+    copy $dir $dest_old
+    copy $dest_new $dir
+}
+
+prepare_dir "cugetari_total"
+prepare_dir "istorii"
+prepare_dir "marturii"
+prepare_dir "ioan"
+prepare_dir "cantari"
+prepare_dir "hristos_puterea"
+prepare_dir "trifa_30_carti"
+prepare_dir "test_results"
