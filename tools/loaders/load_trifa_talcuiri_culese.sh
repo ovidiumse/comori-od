72467edb7fb58981fa75e9fedd0e7391bfe1eba8
Ovidiu Moscaliuc <moscaliuc_ovidiu@yahoo.com>
2021-06-01 10:49:18 +0300

    Amended logged book title for 'Tâlcuiri culese din ziare' loader script

--- tools/loaders/load_trifa_talcuiri_culese.sh
+++ tools/loaders/load_trifa_talcuiri_culese.sh
@@ -10,17 +10,17 @@ if [[ -z "${API_TOTP_KEY}" ]]; then
     export API_TOTP_KEY=${API_TOTP_KEY}
 fi
 
-echo "Fixing Trifa - Talcuirea Evangheliilor..."
+echo "Fixing Trifa - Tâlcuiri culese din ziare..."
 ${TOOLS_DIR}/od-fix.py -i ${DATA_DIR}/trifa_talcuiri_culese/trifa_talcuiri_culese.htm -c ${CFG_DIR}/trifa_talcuiri_culese.yaml
 
-echo "Extracting Trifa - Talcuirea Evangheliilor..."
+echo "Extracting Trifa - Tâlcuiri culese din ziare..."
 ${TOOLS_DIR}/od-extract.py -i ${DATA_DIR}/trifa_talcuiri_culese/trifa_talcuiri_culese_fixed.htm -c ${CFG_DIR}/trifa_talcuiri_culese.yaml -a "Pr. Iosif Trifa" -v "Culese din ziare" -b "Tâlcuiri culese din ziare" -e ${DATA_DIR}/trifa_talcuiri_culese/trifa_talcuiri_culese.json
 
-echo "Post-processing Trifa - Talcuirea Evangheliilor..."
+echo "Post-processing Trifa - Tâlcuiri culese din ziare..."
 ${TOOLS_DIR}/od_postprocess/od_postprocess.py -i ${DATA_DIR}/trifa_talcuiri_culese/trifa_talcuiri_culese.json
 
-echo "Removing existing Trifa - Talcuirea Evangheliilor using '$@' flags..."
+echo "Removing existing Trifa - Tâlcuiri culese din ziare using '$@' flags..."
 ${TOOLS_DIR}/od-remove.py --volume "Talcuirea Evangheliilor" $@
 
-echo "Uploading Trifa - Talcuirea Evangheliilor..."
+echo "Uploading Trifa - Tâlcuiri culese din ziare..."
 ${TOOLS_DIR}/od-upload.py -i ${DATA_DIR}/trifa_talcuiri_culese/trifa_talcuiri_culese_processed.json $@
