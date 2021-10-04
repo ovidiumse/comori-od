e650d9785610b50eff1e3327897e738fc256c631
Ovidiu Moscaliuc <moscaliuc_ovidiu@yahoo.com>
2021-10-04 15:39:30 +0300

    Use native build for BETA

--- web/comori-od-all/build-beta.sh
+++ web/comori-od-all/build-beta.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/sh
 
-export DOCKER_BUILDKIT=1
-export COMPOSE_DOCKER_CLI_BUILD=1
-docker-compose -f docker-compose-beta.yaml --env-file .env-prod build $@
+docker-compose --context comori-od -f docker-compose-beta.yaml --env-file .env-prod build $@
