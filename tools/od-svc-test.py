14f967545754fdeb74c3ada1a4c5551af1002114
Ovidiu Moscaliuc <moscaliuc_ovidiu@yahoo.com>
2021-10-07 10:24:44 +0300

    Made base url configurable for od-svc-test.py

--- tools/od-svc-test.py
+++ tools/od-svc-test.py
@@ -9,6 +9,8 @@ import jwt
 from datetime import datetime, timedelta, timezone
 from dotenv import load_dotenv
 
+global BASE_URL
+
 class Client:
     def __init__(self, baseUrl):
         self.baseUrl = baseUrl
@@ -44,7 +46,8 @@ class Client:
 
 class TestFavorites(unittest.TestCase):
     def setUp(self):
-        self.client_ = Client("http://localhost:9000/od/")
+        self.client_ = Client(BASE_URL)
+        print(f"Client initialized with BASE_URL: {BASE_URL}")
 
     def generateFav(self):
         random.seed()
@@ -107,11 +110,16 @@ class TestFavorites(unittest.TestCase):
 if __name__ == "__main__":
     args = sys.argv[1:]
     if not args:
-        print("Usage: od-svc-test.py [dotenvFilepath]")
+        print("Usage: od-svc-test.py [dotenvFilepath] [baseUrl]")
         exit(1)
     
     dotenv_filePath = args[0]
     if dotenv_filePath:
         load_dotenv(dotenv_filePath)
+
+    if len(args) > 1:
+        BASE_URL = args[1]
+    else:
+        BASE_URL = "http://localhost:9000/od/"
 
     unittest.main(argv=[sys.argv[0]], exit=False)
