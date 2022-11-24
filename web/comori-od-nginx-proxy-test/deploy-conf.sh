499d3cc2c6a46f641ebc3215fa617abf50d46f34
Ovidiu Moscaliuc <moscaliuc_ovidiu@yahoo.com>
2022-11-24 10:01:21 +0200

    Added nginx-proxy package for the test machine

---
+++ web/comori-od-nginx-proxy-test/deploy-conf.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/sh
+
+export $(cat ../comori-od-all/.env-test | xargs)
+docker cp test.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/test.comori-od.ro
+docker cp test.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/localhost
+docker cp testapi.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/testapi.comori-od.ro
+docker cp testbible-api.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/testbible-api.comori-od.ro
+docker cp testmongo.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/testmongo.comori-od.ro
+docker cp testnew.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/testnew.comori-od.ro
+docker cp proxy.conf nginx-proxy:/etc/nginx/conf.d/
+docker restart nginx-proxy
