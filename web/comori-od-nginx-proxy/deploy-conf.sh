d91541f5d241a7b8d84cf14a76119f542c45c92b
Ovidiu Moscaliuc <moscaliuc_ovidiu@yahoo.com>
2022-12-29 21:14:26 +0200

    Added nginx config for grafana and mongo

--- web/comori-od-nginx-proxy/deploy-conf.sh
+++ web/comori-od-nginx-proxy/deploy-conf.sh
@@ -7,7 +7,7 @@ docker cp comori-od.conf nginx-proxy:/etc/nginx/vhost.d/www.comori-od.ro
 docker cp api.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/api.comori-od.ro
 docker cp bible-api.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/bible-api.comori-od.ro
 docker cp mongo.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/mongo.comori-od.ro
-docker cp new.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/new.comori-od.ro
 docker cp kibana.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/kibana.comori-od.ro
+docker cp grafana.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/grafana.comori-od.ro
 docker cp proxy.conf nginx-proxy:/etc/nginx/conf.d/
 docker restart nginx-proxy
