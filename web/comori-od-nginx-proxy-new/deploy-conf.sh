622339c89b43dede518a0befe109174225cd0507
Ovidiu Moscaliuc <moscaliuc_ovidiu@yahoo.com>
2023-02-14 21:40:22 +0200

    nginx-new improvements

--- web/comori-od-nginx-proxy-new/deploy-conf.sh
+++ web/comori-od-nginx-proxy-new/deploy-conf.sh
@@ -7,8 +7,8 @@ docker cp newapi.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/newapi.comori-od.
 docker cp newbible-api.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/newbible-api.comori-od.ro
 docker cp newmongo.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/newmongo.comori-od.ro
 docker cp newnew.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/newnew.comori-od.ro
-docker cp newkibana.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/newkibana.comori-od.ro
 docker cp proxy.conf nginx-proxy:/etc/nginx/conf.d/
+docker cp nginx.tmpl nginx-proxy:/app/
 
 docker cp test.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/test.comori-od.ro
 docker cp test.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/testapi.comori-od.ro
