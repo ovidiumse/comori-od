eb71dac7e16278359987e72ad3966e5bcefdca95
Ovidiu Moscaliuc <moscaliuc_ovidiu@yahoo.com>
2023-02-27 12:05:20 +0200

    Adapted docker build scripts for the new server

--- web/comori-od-nginx-proxy-new/deploy-conf.sh
+++ web/comori-od-nginx-proxy-new/deploy-conf.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/sh
 
 export $(cat ../comori-od-all/.env-new | xargs)
-docker cp new.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/new.comori-od.ro
-docker cp new.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/localhost
-docker cp newapi.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/newapi.comori-od.ro
-docker cp newbible-api.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/newbible-api.comori-od.ro
-docker cp newmongo.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/newmongo.comori-od.ro
-docker cp newnew.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/newnew.comori-od.ro
+docker cp comori-od.conf nginx-proxy:/etc/nginx/vhost.d/comori-od.ro
+docker cp comori-od.conf nginx-proxy:/etc/nginx/vhost.d/localhost
+docker cp comori-od.conf nginx-proxy:/etc/nginx/vhost.d/www.comori-od.ro
+docker cp api.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/api.comori-od.ro
+docker cp bible-api.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/bible-api.comori-od.ro
+docker cp mongo.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/mongo.comori-od.ro
+# docker cp new.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/new.comori-od.ro
 docker cp proxy.conf nginx-proxy:/etc/nginx/conf.d/
 docker cp nginx.tmpl nginx-proxy:/app/
 
