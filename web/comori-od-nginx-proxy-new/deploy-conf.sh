c30f6c8521ffaf292d219b93d9e8bad2adab089f
Ovidiu Moscaliuc <moscaliuc_ovidiu@yahoo.com>
2023-06-14 19:53:27 +0300

    Added meet, talk, element, matrix, turn as dummy containers for nginx reverse-proxy routing

--- web/comori-od-nginx-proxy-new/deploy-conf.sh
+++ web/comori-od-nginx-proxy-new/deploy-conf.sh
@@ -5,6 +5,11 @@ docker cp comori-od.conf nginx-proxy:/etc/nginx/vhost.d/www.comori-od.ro
 docker cp api.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/api.comori-od.ro
 docker cp bible-api.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/bible-api.comori-od.ro
 docker cp mongo.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/mongo.comori-od.ro
+docker cp meet.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/meet.comori-od.ro
+docker cp talk.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/talk.comori-od.ro
+docker cp element.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/element.comori-od.ro
+docker cp matrix.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/matrix.comori-od.ro
+docker cp turn.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/turn.comori-od.ro
 docker cp new.comori-od.conf nginx-proxy:/etc/nginx/vhost.d/
 
 docker cp proxy.conf nginx-proxy:/etc/nginx/conf.d/
