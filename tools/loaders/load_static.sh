3f5191c6928381dd304f80728f4993a6e56d7855
Ovidiu Moscaliuc <moscaliuc_ovidiu@yahoo.com>
2023-10-04 09:29:54 +0000

    Added hook for books conversion

--- tools/loaders/load_static.sh
+++ tools/loaders/load_static.sh
@@ -9,7 +9,5 @@ DATA_DIR=${CWD}/../../data
 IMG_DIR=${CWD}/../../img
 TOOLS_DIR=${CWD}/../
 
-ARGS=$@
-
-${TOOLS_DIR}/od-static-upload.py --input-dir ${DATA_DIR}/uploaded --dest-dir ${IDX_NAME}/data/ ${ARGS}
-${TOOLS_DIR}/od-static-upload.py --input-dir ${IMG_DIR} --dest-dir ${IDX_NAME}/img/ ${ARGS}
+${TOOLS_DIR}/od-static-upload.py --input-dir ${DATA_DIR}/uploaded --dest-dir od/data/
+${TOOLS_DIR}/od-static-upload.py --input-dir ${IMG_DIR} --dest-dir od/img/
