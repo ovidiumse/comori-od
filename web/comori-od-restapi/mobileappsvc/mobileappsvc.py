424ffb830eba6350df8a129622a0e8c3667f075a
Ovidiu Moscaliuc <moscaliuc_ovidiu@yahoo.com>
2024-01-11 19:34:21 +0000

    Increased JWT verification leeway from 10 to 120 seconds

--- web/comori-od-restapi/mobileappsvc/mobileappsvc.py
+++ web/comori-od-restapi/mobileappsvc/mobileappsvc.py
@@ -16,7 +16,7 @@ class MobileAppService(object):
     
     @timeit("Authorizing", __name__)
     def authorize(self, authorization):
-        return jwt.decode(authorization, self.public_key_ecdsa, algorithms='ES512', options={"verify_exp": True}, leeway=timedelta(seconds=10))
+        return jwt.decode(authorization, self.public_key_ecdsa, algorithms='ES512', options={"verify_exp": True}, leeway=timedelta(seconds=120))
 
     def getUserId(self, auth):
         return "{}.{}".format(auth['sub'], auth['iss'])
